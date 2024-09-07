6c6b2584fca1ce5bece6c90ce23b64dc3860735b
jenlesamuel <jenlesamuel@gmail.com>
2024-09-08 00:00:26 +0100

    Implemented login endpoint

---
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request) {
+        $credentials = $request->only("email","password");
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid credentials',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'token' => $token,
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,7 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function() {
-    Route::post('/register', [RegisterController::class, 'register'])->name('aggregator.register');
+    Route::post('/register', [RegisterController::class, 'register']);
+    Route::post('/login', [LoginController::class, 'login']);
 });
 
-//Route::post('/register', [RegisterController::class, 'register'])->name('aggregator.register');
+
